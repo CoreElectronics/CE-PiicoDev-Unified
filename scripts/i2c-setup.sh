047f3fdc00cc3e3619a6a08a4040d3ec70f99e44
CoreProduction2 <production2.inbox@coreelectronics.com.au>
2022-10-07 13:27:18 +1100

    Update i2c-setup.sh

--- scripts/i2c-setup.sh
+++ scripts/i2c-setup.sh
@@ -1,14 +1,14 @@
 echo This script will modify the /boot/config.txt file to enable I2C and set the correct baudrate (400k). 
-echo if you want to view the contents of this script, save it to a file. Run: curl -L https://piico.dev/i2c-setup >> i2c-setup.sh
+echo if you want to view the contents of this script, save it to a file. Run: curl -L https://piico.dev/i2csetup >> i2c-setup.sh
 while true
 do
       read -r -p "Do you want to procees? [y/n]" input
  
       case $input in
             [yY][eE][sS]|[yY])
-                  uudo sed -i.bak '/^dtparam=i2c_arm=' /boot/config.txt                                                                                # remove dtparam=i2c_arm=
-                  sudo sed -i '/^dtparam=i2c_arm_baudrate=/d' /boot/config.txt                                                                         # remove dtparam=i2c_arm_baudrate=
-                  sudo sed -i '$a# Added by PiicoDev: require I2C 400k baudrate\ndtparam=i2c_arm=on\ndtparam=i2c_arm_baudrate=400000' /boot/config.txt # add dtparam=i2c_arm=on and dtparam=i2c_arm_baudrate=400000
+                  sed -i.bak '/^dtparam=i2c_arm=' /boot/config.txt                                                                                # remove dtparam=i2c_arm=
+                  sed -i '/^dtparam=i2c_arm_baudrate=/d' /boot/config.txt                                                                         # remove dtparam=i2c_arm_baudrate=
+                  sed -i '$a# Added by PiicoDev: require I2C 400k baudrate\ndtparam=i2c_arm=on\ndtparam=i2c_arm_baudrate=400000' /boot/config.txt # add dtparam=i2c_arm=on and dtparam=i2c_arm_baudrate=400000
                   echo Setup complete. Please reboot.
                   break
                   ;;
