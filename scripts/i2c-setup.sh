280dd061c08aa080c3da3cb073efef4176360ebc
CoreProduction2 <production2.inbox@coreelectronics.com.au>
2022-10-13 13:22:43 +1100

    Update i2c-setup.sh

--- scripts/i2c-setup.sh
+++ scripts/i2c-setup.sh
@@ -5,13 +5,13 @@ echo "The script will run in 10 seconds.  Hit CTRL+C to abort"
 sleep 10
 
 # remove dtparam=i2c_arm=
-sed -i.bak '/^dtparam=i2c_arm=/d' /boot/config.txt
+sudo sed -i.bak '/^dtparam=i2c_arm=/d' /boot/config.txt
 # remove dtparam=i2c_arm_baudrate=
-sed -i '/^dtparam=i2c_arm_baudrate=/d' /boot/config.txt
+sudo sed -i '/^dtparam=i2c_arm_baudrate=/d' /boot/config.txt
 # remove comment
-sed -i '/^# Added by PiicoDev:/d' /boot/config.txt
+sudo sed -i '/^# Added by PiicoDev:/d' /boot/config.txt
 # add dtparam=i2c_arm=on and dtparam=i2c_arm_baudrate=400000
-sed -i '$a# Added by PiicoDev: require I2C and 400k baudrate\ndtparam=i2c_arm=on\ndtparam=i2c_arm_baudrate=400000' /boot/config.txt
+sudo sed -i '$a# Added by PiicoDev: require I2C and 400k baudrate\ndtparam=i2c_arm=on\ndtparam=i2c_arm_baudrate=400000' /boot/config.txt
 echo Setup complete. Please reboot.
 
 done
